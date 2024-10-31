186bfd78428763eccff78f77053c101b7db8647b
Carlos Ramirez Diaz <cerd469@gmail.com>
2024-10-31 20:45:17 -0300

    DBContext y Program

---
+++ Infraestructure/Data/PadelDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure.Data
+{
+    public class PadelDbContext : DbContext
+    {
+        public PadelDbContext(DbContextOptions<PadelDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+        public DbSet<Cancha> Canchas { get; set; }
+        public DbSet<Club> Clubes { get; set; }
+        public DbSet<Turno> Turnos { get; set; }
+    }
+}
--- TurnosPadel/Program.cs
+++ TurnosPadel/Program.cs
@@ -1,4 +1,13 @@
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PadelDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
@@ -6,6 +15,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IReservaRepository, ReservaRepository>();
+builder.Services.AddScoped<ICanchaRepository, CanchaRepository>();
+builder.Services.AddScoped<IClubRepository, ClubRepository>();
+builder.Services.AddScoped<ClubService>();
+builder.Services.AddScoped<UsuarioService>();
+builder.Services.AddScoped<ReservaService>();
+builder.Services.AddScoped<CanchaService>();
+
 
 var app = builder.Build();
 
